9fcda32ba350b16090aea2fae1d8ea8a7404e2bb
Miguel2236 <miguelm2236@gmail.com>
2021-01-18 08:56:53 -0600

    calculadora con operaciones basicas

---
+++ calculadora/Form1.Designer.vb
@@ -0,0 +1,329 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtPantalla = New System.Windows.Forms.TextBox()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.btnBorrar = New System.Windows.Forms.Button()
+        Me.btnPorcentaje = New System.Windows.Forms.Button()
+        Me.btnMasMenos = New System.Windows.Forms.Button()
+        Me.btnRaiz = New System.Windows.Forms.Button()
+        Me.btnExp = New System.Windows.Forms.Button()
+        Me.btnDivicion = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btnInversa = New System.Windows.Forms.Button()
+        Me.btnMultiplicacion = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btnResta = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btnIgual = New System.Windows.Forms.Button()
+        Me.btnSuma = New System.Windows.Forms.Button()
+        Me.btnPunto = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtPantalla
+        '
+        Me.txtPantalla.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtPantalla.Font = New System.Drawing.Font("Consolas", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPantalla.Location = New System.Drawing.Point(35, 27)
+        Me.txtPantalla.Multiline = True
+        Me.txtPantalla.Name = "txtPantalla"
+        Me.txtPantalla.Size = New System.Drawing.Size(281, 39)
+        Me.txtPantalla.TabIndex = 0
+        Me.txtPantalla.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Location = New System.Drawing.Point(24, 95)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(60, 45)
+        Me.btnLimpiar.TabIndex = 1
+        Me.btnLimpiar.Text = "CE"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'btnBorrar
+        '
+        Me.btnBorrar.Location = New System.Drawing.Point(90, 95)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Size = New System.Drawing.Size(57, 45)
+        Me.btnBorrar.TabIndex = 2
+        Me.btnBorrar.Text = "<--"
+        Me.btnBorrar.UseVisualStyleBackColor = True
+        '
+        'btnPorcentaje
+        '
+        Me.btnPorcentaje.Location = New System.Drawing.Point(153, 95)
+        Me.btnPorcentaje.Name = "btnPorcentaje"
+        Me.btnPorcentaje.Size = New System.Drawing.Size(54, 45)
+        Me.btnPorcentaje.TabIndex = 3
+        Me.btnPorcentaje.Text = "%"
+        Me.btnPorcentaje.UseVisualStyleBackColor = True
+        '
+        'btnMasMenos
+        '
+        Me.btnMasMenos.Location = New System.Drawing.Point(213, 95)
+        Me.btnMasMenos.Name = "btnMasMenos"
+        Me.btnMasMenos.Size = New System.Drawing.Size(54, 45)
+        Me.btnMasMenos.TabIndex = 4
+        Me.btnMasMenos.Text = "+/-"
+        Me.btnMasMenos.UseVisualStyleBackColor = True
+        '
+        'btnRaiz
+        '
+        Me.btnRaiz.Location = New System.Drawing.Point(273, 95)
+        Me.btnRaiz.Name = "btnRaiz"
+        Me.btnRaiz.Size = New System.Drawing.Size(54, 45)
+        Me.btnRaiz.TabIndex = 5
+        Me.btnRaiz.Text = "√"
+        Me.btnRaiz.UseVisualStyleBackColor = True
+        '
+        'btnExp
+        '
+        Me.btnExp.Location = New System.Drawing.Point(273, 146)
+        Me.btnExp.Name = "btnExp"
+        Me.btnExp.Size = New System.Drawing.Size(54, 45)
+        Me.btnExp.TabIndex = 10
+        Me.btnExp.Text = "^"
+        Me.btnExp.UseVisualStyleBackColor = True
+        '
+        'btnDivicion
+        '
+        Me.btnDivicion.Location = New System.Drawing.Point(213, 146)
+        Me.btnDivicion.Name = "btnDivicion"
+        Me.btnDivicion.Size = New System.Drawing.Size(54, 45)
+        Me.btnDivicion.TabIndex = 9
+        Me.btnDivicion.Text = "/"
+        Me.btnDivicion.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Location = New System.Drawing.Point(153, 146)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(54, 45)
+        Me.btn9.TabIndex = 8
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Location = New System.Drawing.Point(90, 146)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(57, 45)
+        Me.btn8.TabIndex = 7
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Location = New System.Drawing.Point(24, 146)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(60, 45)
+        Me.btn7.TabIndex = 6
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btnInversa
+        '
+        Me.btnInversa.Location = New System.Drawing.Point(273, 197)
+        Me.btnInversa.Name = "btnInversa"
+        Me.btnInversa.Size = New System.Drawing.Size(54, 45)
+        Me.btnInversa.TabIndex = 15
+        Me.btnInversa.Text = "1/x"
+        Me.btnInversa.UseVisualStyleBackColor = True
+        '
+        'btnMultiplicacion
+        '
+        Me.btnMultiplicacion.Location = New System.Drawing.Point(213, 197)
+        Me.btnMultiplicacion.Name = "btnMultiplicacion"
+        Me.btnMultiplicacion.Size = New System.Drawing.Size(54, 45)
+        Me.btnMultiplicacion.TabIndex = 14
+        Me.btnMultiplicacion.Text = "X"
+        Me.btnMultiplicacion.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Location = New System.Drawing.Point(153, 197)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(54, 45)
+        Me.btn6.TabIndex = 13
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(90, 197)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(57, 45)
+        Me.btn5.TabIndex = 12
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(24, 197)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(60, 45)
+        Me.btn4.TabIndex = 11
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btnResta
+        '
+        Me.btnResta.Location = New System.Drawing.Point(213, 248)
+        Me.btnResta.Name = "btnResta"
+        Me.btnResta.Size = New System.Drawing.Size(54, 45)
+        Me.btnResta.TabIndex = 19
+        Me.btnResta.Text = "-"
+        Me.btnResta.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(153, 248)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(54, 45)
+        Me.btn3.TabIndex = 18
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(90, 248)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(57, 45)
+        Me.btn2.TabIndex = 17
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(24, 248)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(60, 45)
+        Me.btn1.TabIndex = 16
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btnIgual
+        '
+        Me.btnIgual.Location = New System.Drawing.Point(273, 248)
+        Me.btnIgual.Name = "btnIgual"
+        Me.btnIgual.Size = New System.Drawing.Size(54, 96)
+        Me.btnIgual.TabIndex = 25
+        Me.btnIgual.Text = "="
+        Me.btnIgual.UseVisualStyleBackColor = True
+        '
+        'btnSuma
+        '
+        Me.btnSuma.Location = New System.Drawing.Point(213, 299)
+        Me.btnSuma.Name = "btnSuma"
+        Me.btnSuma.Size = New System.Drawing.Size(54, 45)
+        Me.btnSuma.TabIndex = 24
+        Me.btnSuma.Text = "+"
+        Me.btnSuma.UseVisualStyleBackColor = True
+        '
+        'btnPunto
+        '
+        Me.btnPunto.Location = New System.Drawing.Point(153, 299)
+        Me.btnPunto.Name = "btnPunto"
+        Me.btnPunto.Size = New System.Drawing.Size(54, 45)
+        Me.btnPunto.TabIndex = 23
+        Me.btnPunto.Text = "."
+        Me.btnPunto.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Location = New System.Drawing.Point(24, 299)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(123, 45)
+        Me.btn0.TabIndex = 21
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(351, 412)
+        Me.Controls.Add(Me.btnIgual)
+        Me.Controls.Add(Me.btnSuma)
+        Me.Controls.Add(Me.btnPunto)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnResta)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btnInversa)
+        Me.Controls.Add(Me.btnMultiplicacion)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btnExp)
+        Me.Controls.Add(Me.btnDivicion)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btnRaiz)
+        Me.Controls.Add(Me.btnMasMenos)
+        Me.Controls.Add(Me.btnPorcentaje)
+        Me.Controls.Add(Me.btnBorrar)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Controls.Add(Me.txtPantalla)
+        Me.Name = "Form1"
+        Me.Text = "Mi Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtPantalla As TextBox
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents btnBorrar As Button
+    Friend WithEvents btnPorcentaje As Button
+    Friend WithEvents btnMasMenos As Button
+    Friend WithEvents btnRaiz As Button
+    Friend WithEvents btnExp As Button
+    Friend WithEvents btnDivicion As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btnInversa As Button
+    Friend WithEvents btnMultiplicacion As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btnResta As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btnIgual As Button
+    Friend WithEvents btnSuma As Button
+    Friend WithEvents btnPunto As Button
+    Friend WithEvents btn0 As Button
+End Class
---
+++ calculadora/Form1.vb
@@ -0,0 +1,194 @@
+﻿Public Class Form1
+    Private memoria1 As Double = 0.0
+    Private memoria2 As Double = 0.0
+    Private signo As String
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        txtPantalla.Text = txtPantalla.Text & "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        txtPantalla.Text = txtPantalla.Text & "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        txtPantalla.Text = txtPantalla.Text & "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        txtPantalla.Text = txtPantalla.Text & "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        txtPantalla.Text = txtPantalla.Text & "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        txtPantalla.Text = txtPantalla.Text & "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        txtPantalla.Text = txtPantalla.Text & "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        txtPantalla.Text = txtPantalla.Text & "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        txtPantalla.Text = txtPantalla.Text & "9"
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        txtPantalla.Text = txtPantalla.Text & "0"
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        txtPantalla.Text = ""
+        memoria1 = 0.0
+        memoria2 = 0.0
+        signo = String.Empty
+    End Sub
+
+    Private Sub btnPunto_Click(sender As Object, e As EventArgs) Handles btnPunto.Click
+        If txtPantalla.Text = "" Then
+            txtPantalla.Text = txtPantalla.Text = "0."
+        ElseIf validar_punto(txtPantalla.Text) = False Then
+            txtPantalla.Text = txtPantalla.Text & "."
+        End If
+    End Sub
+
+    Private Sub btnSuma_Click(sender As Object, e As EventArgs) Handles btnSuma.Click
+        Try
+            If txtPantalla.Text <> "" Then
+                memoria1 = Val(txtPantalla.Text)
+                signo = "+"
+                txtPantalla.Clear()
+            End If
+        Catch ex As Exception
+            txtPantalla.Text = "Error"
+        End Try
+    End Sub
+
+    Private Sub btnResta_Click(sender As Object, e As EventArgs) Handles btnResta.Click
+        Try
+            If txtPantalla.Text <> "" Then
+                memoria1 = Val(txtPantalla.Text)
+                signo = "-"
+                txtPantalla.Clear()
+            End If
+        Catch ex As Exception
+            txtPantalla.Text = "Error"
+        End Try
+    End Sub
+
+    Private Sub btnMultiplicacion_Click(sender As Object, e As EventArgs) Handles btnMultiplicacion.Click
+        Try
+            If txtPantalla.Text <> "" Then
+                memoria1 = Val(txtPantalla.Text)
+                signo = "*"
+                txtPantalla.Clear()
+            End If
+        Catch ex As Exception
+            txtPantalla.Text = "Error"
+        End Try
+    End Sub
+
+    Private Sub btnDivicion_Click(sender As Object, e As EventArgs) Handles btnDivicion.Click
+        Try
+            If txtPantalla.Text <> "" Then
+                memoria1 = Val(txtPantalla.Text)
+                signo = "/"
+                txtPantalla.Clear()
+            End If
+        Catch ex As Exception
+            txtPantalla.Text = "Error"
+        End Try
+    End Sub
+
+    Private Sub btnPorcentaje_Click(sender As Object, e As EventArgs) Handles btnPorcentaje.Click
+        Try
+            If txtPantalla.Text <> "" Then
+                memoria1 = Val(txtPantalla.Text)
+                signo = "%"
+                txtPantalla.Clear()
+            End If
+        Catch ex As Exception
+            txtPantalla.Text = "Error"
+        End Try
+    End Sub
+
+    Private Sub btnRaiz_Click(sender As Object, e As EventArgs) Handles btnRaiz.Click
+        Try
+            If txtPantalla.Text <> "" Then
+                memoria1 = Val(txtPantalla.Text)
+                signo = "raiz"
+                txtPantalla.Clear()
+            End If
+        Catch ex As Exception
+            txtPantalla.Text = "Error"
+        End Try
+    End Sub
+
+    Private Sub btnExp_Click(sender As Object, e As EventArgs) Handles btnExp.Click
+        Try
+            If txtPantalla.Text <> "" Then
+                memoria1 = Val(txtPantalla.Text)
+                signo = "exp"
+                txtPantalla.Clear()
+            End If
+        Catch ex As Exception
+            txtPantalla.Text = "Error"
+        End Try
+    End Sub
+
+    Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
+        Try
+            Dim largo As Integer
+            If txtPantalla.Text <> "" Then
+                largo = txtPantalla.Text.Length
+                txtPantalla.Text = Mid(txtPantalla.Text, 1, largo - 1)
+            End If
+        Catch ex As Exception
+            txtPantalla.Text = "Error"
+        End Try
+    End Sub
+
+    Private Sub btnMasMenos_Click(sender As Object, e As EventArgs) Handles btnMasMenos.Click
+        Try
+            Dim largo As Integer
+            If txtPantalla.Text <> "" Then
+                txtPantalla.Text = txtPantalla.Text * (-1)
+            End If
+        Catch ex As Exception
+            txtPantalla.Text = "Error"
+        End Try
+    End Sub
+
+    Private Sub btnInversa_Click(sender As Object, e As EventArgs) Handles btnInversa.Click
+        Try
+            Dim largo As Integer
+            If txtPantalla.Text <> "" Then
+                txtPantalla.Text = 1 / txtPantalla.Text
+            End If
+        Catch ex As Exception
+            txtPantalla.Text = "Error"
+        End Try
+    End Sub
+
+    Private Function validar_punto(ByVal cadena As String) As Boolean
+        Dim largo As Integer
+        Dim respuesta As Boolean = False
+
+        largo = cadena.Length
+
+        For i Ad Integer = 1 To largo Step 1
+            If Mid(cadena, i, 1) = "." Then
+                respuesta = True
+            End If
+
+        Next
+        Return respuesta
+    End Function
+
+End Class
